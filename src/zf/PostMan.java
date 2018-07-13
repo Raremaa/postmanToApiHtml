6b159b41fe946570647c1128c44d1666d7be9012
masaiqi <masaiqi1996@icloud.com>
2018-07-13 17:52:19 +0800

    1

---
+++ src/zf/PostMan.java
@@ -0,0 +1,46 @@
+package zf;
+
+import java.util.List;
+
+/**
+ * Created by Yan on 2018/4/3.
+ */
+public class PostMan {
+
+    private String name;
+    private String url;
+    private String method;
+    private List<Data> data;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public List<Data> getData() {
+        return data;
+    }
+
+    public void setData(List<Data> data) {
+        this.data = data;
+    }
+}
